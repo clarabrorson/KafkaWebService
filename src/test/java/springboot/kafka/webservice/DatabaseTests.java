226db6be1151122f9d1257a03a6b52917e2d5342
clarabrorson <88clabro@gafe.molndal.se>
2023-09-20 15:00:48 +0200

    Update DatabaseTests.java

--- src/test/java/springboot/kafka/webservice/DatabaseTests.java
+++ src/test/java/springboot/kafka/webservice/DatabaseTests.java
@@ -6,8 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import springboot.kafka.webservice.payload.Book;
 import springboot.kafka.webservice.repository.BookRepository;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
@@ -15,6 +14,9 @@ public class DatabaseTests {
 
     @Autowired
     BookRepository bookRepository;
+
+    static Book testBook; // ta bort(om du vill återställa testerna så att de kan köra enskilt)
+
     @BeforeEach
     void setUp() {
         System.out.println("Before each test");
@@ -25,6 +27,12 @@ public class DatabaseTests {
         System.out.println("After each test");
     }
 
+    @AfterAll
+    static void afterAll() {
+
+        System.out.println("After all tests");
+    }
+
     @Test
     @Order(1)
     void createBook() {
@@ -33,30 +41,34 @@ public class DatabaseTests {
         book.setTitle("B");
         book.setGenre("C");
         //Sparar bok i DB
-        bookRepository.save(book);
+        //bookRepository.save(book);
+        testBook = bookRepository.save(book); // ta bort(om du vill återställa testerna så att de kan köra enskilt)
 
         //Kontrollera att boken finns i databasen
-        assertNotNull(bookRepository.findById(1L).get());
+        //assertNotNull(bookRepository.findById(1L).get());
+        assertNotNull(bookRepository.findById(testBook.getId()).get().getAuthor());// ta bort(om du vill återställa testerna så att de kan köra enskilt)
     }
 
     @Test
     @Order(2)
     void updateBook() {
         //Hämta bok med id 1
-        Book fetchedBook = bookRepository.findById(1L).get();
+        Book fetchedBook = bookRepository.findById(testBook.getId()).get();
         assertNotNull(fetchedBook);
 
         fetchedBook.setAuthor("D");
         bookRepository.save(fetchedBook);
         //Kontrollera att boken har uppdaterats
-        assertNotNull(bookRepository.findById(1L).get().getAuthor());
+        //assertNotNull(bookRepository.findById(1L).get().getAuthor());
+        assertEquals("D", bookRepository.findById(testBook.getId()).get().getAuthor()); // ta bort(om du vill återställa testerna så att de kan köra enskilt
     }
 
     @Test
     @Order(3)
     void removeBook() {
-        assertNotNull(bookRepository.findById(1L).get());
-        bookRepository.deleteById(1L);
-        assertTrue(bookRepository.findById(1L).isEmpty());
+        //assertNotNull(bookRepository.findById(1L).get());
+        assertNotNull(bookRepository.findById(testBook.getId()).get()); // ta bort(om du vill återställa testerna så att de kan köra enskilt)
+        bookRepository.deleteById(testBook.getId()); // ta bort(om du vill återställa testerna så att de kan köra enskilt)
+        assertTrue(bookRepository.findById(testBook.getId()).isEmpty()); // ta bort(om du vill återställa testerna så att de kan köra enskilt)
     }
 }
