22829851cdf3955e2acda91d9a078d656637ac41
clarabrorson <88clabro@gafe.molndal.se>
2023-09-21 09:10:35 +0200

    Update BookRepository.java

--- src/main/java/springboot/kafka/webservice/repository/BookRepository.java
+++ src/main/java/springboot/kafka/webservice/repository/BookRepository.java
@@ -4,6 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import springboot.kafka.webservice.payload.Book;
 
+/**
+ * BookRepository
+ * This interface is used to perform database operations on the Book table
+ * It extends JpaRepository which contains all the methods to perform CRUD operations on the Book table
+ * JpaRepository takes two parameters, the entity class and the primary key type of the entity class
+ */
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long>{
 }
